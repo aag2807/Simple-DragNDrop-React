f9003df4b10800a0acb745d04fed69b52ca95fa1
aag2807 <aagn2807@gmail.com>
2021-01-13 23:17:32 -0400

    cleaned code

--- src/components/Ticket/Ticket.js
+++ src/components/Ticket/Ticket.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
 const TicketWrapper = styled.div`
     background: darkGray;
@@ -8,28 +8,26 @@ const TicketWrapper = styled.div`
 
     &:not(:last-child) {
         margin-bottom: 5%;
-        margin-right: ${props => !!props.marginRight ? '1%' : '0'}
+        margin-right: ${(props) => (!!props.marginRight ? "1%" : "0")}
 `;
 
 const Title = styled.h3`
-    width: 100%;
-    margin: 0px;
+  width: 100%;
+  margin: 0px;
 `;
 
 const Body = styled.p`
-    width: 100%;
+  width: 100%;
 `;
 
-
-
 const Ticket = ({ marginRight, onDragStart, ticket }) => (
-    <TicketWrapper
-      draggable
-      onDragStart={e => onDragStart && onDragStart(e, ticket.id)}
-      marginRight={marginRight}
-    >
-      <Title>{ticket.title}</Title>
-      <Body>{ticket.body}</Body>
-    </TicketWrapper>
-  );
+  <TicketWrapper
+    draggable
+    onDragStart={(e) => onDragStart && onDragStart(e, ticket.id)}
+    marginRight={marginRight}
+  >
+    <Title>{ticket.title}</Title>
+    <Body>{ticket.body}</Body>
+  </TicketWrapper>
+);
 export default Ticket;
